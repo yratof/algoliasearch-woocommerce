7db870ff0456de27b51a2ecf486161ce4eeb4806
Raymond Rutjes <raymond.rutjes@gmail.com>
2016-09-14 13:16:26 +0200

    Push WooCommerce attributes

--- algolia-woocommerce.php
+++ algolia-woocommerce.php
@@ -5,3 +5,57 @@
  * Plugin Name: Algolia Search for WooCommerce
  */
 
+
+/**
+ * If Algolia is not active, let users know.
+ **/
+if ( ! in_array( 'algolia/algolia.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+	add_action( 'admin_notices', function() {
+		echo '<div class="error notice">
+			  	<p>' . esc_html__( 'Algolia Search for WooCommerce: Algolia Search plugin should be enabled.', 'algolia-woocommerce' ) . '</p>
+		  	  </div>';
+	} );
+}
+
+/**
+ * If WooCommerce is not active, let users know.
+ **/
+if ( ! in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+	add_action( 'admin_notices', function() {
+		echo '<div class="error notice">
+			  	<p>' . esc_html__( 'Algolia Search for WooCommerce: WooCommerce plugin should be enabled.', 'algolia-woocommerce' ) . '</p>
+		  	  </div>';
+	} );
+}
+
+/**
+ * @param array   $attributes
+ * @param WP_Post $post
+ *
+ * @return array
+ */
+function aw_product_shared_attributes( array $attributes, WP_Post $post ) {
+	$product = wc_get_product( $post );
+
+	$attributes['product_type'] = (string) $product->get_type();
+	$attributes['display_price'] = (float) $product->get_display_price();
+	$attributes['price'] = (float) $product->get_price();
+	$attributes['price_excluding_tax'] = (float) $product->get_price_excluding_tax();
+	$attributes['regular_price'] = (float) $product->get_regular_price();
+	$attributes['sale_price'] = (float) $product->get_sale_price();
+	$attributes['sale_price'] = (float) $product->get_price();
+	$attributes['average_rating'] = (float) $product->get_average_rating();
+	$attributes['rating_count'] = (int) $product->get_rating_count();
+	$attributes['attributes'] = (array) $product->get_attributes();
+	$attributes['width'] = (string) $product->get_width();
+	$attributes['height'] = (string) $product->get_height();
+	$attributes['weight'] = (string) $product->get_weight();
+	$attributes['length'] = (string) $product->get_length();
+	$attributes['review_count'] = (int) $product->get_review_count();
+	$attributes['dimensions'] = (string) $product->get_dimensions();
+
+	return $attributes;
+}
+
+add_filter( 'algolia_post_product_shared_attributes', 'aw_product_shared_attributes', 10, 2 );
+add_filter( 'algolia_searchable_post_product_shared_attributes', 'aw_product_shared_attributes', 10, 2 );
