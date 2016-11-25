00599770cbd8167b41a8b8e1325332c8cac46ca7
Raymond Rutjes <raymond.rutjes@gmail.com>
2016-11-25 15:56:07 +0100

    Add color picker options for later use

--- includes/admin/views/appearance.php
+++ includes/admin/views/appearance.php
@@ -19,6 +19,21 @@
 
 <script>
 	jQuery(document).ready(function($){
-		$('.color-picker').wpColorPicker();
+		var options = {
+			// you can declare a default color here,
+			// or in the data-default-color attribute on the input
+			defaultColor: '<?php echo esc_html( $primary_color ); ?>',
+			// a callback to fire whenever the color changes to a valid color
+			change: function(event, ui){},
+			// a callback to fire when the input is emptied or an invalid color
+			clear: function() {},
+			// hide the color picker controls on load
+			hide: true,
+			// show a group of common colors beneath the square
+			// or, supply an array of colors to customize further
+			palettes: true
+		};
+
+		$('.color-picker').wpColorPicker(options);
 	});
 </script>
