5ac8085b90752017540da60824dddcfad05689c3
Raymond Rutjes <raymond.rutjes@gmail.com>
2016-11-28 13:44:08 +0100

    Guess default selector for container
    Also fixed an issue in path computation
    
    Resolves: #16

--- assets/js/selector.js
+++ assets/js/selector.js
@@ -1,7 +1,15 @@
 jQuery(function($) {
   var current_selector = algolia.woocommerce.selector;
+
   if(current_selector.length > 0) {
-    activate(current_selector);
+    activate($(current_selector));
+  } else if(current_selector.length === 0){
+    // Only guess if no selector is set.
+    // We can't presume what manual selector the user might enter.
+    var guess = $('.woocommerce-breadcrumb').parent();
+    if(guess.length > 0) {
+      activate(guess);
+    }
   }
   
   $(document).on('mouseover', 'div, main', function(e) {
@@ -25,19 +33,17 @@ jQuery(function($) {
   
   $(document).on('click', '.algolia-selector', function(e) {
     e.preventDefault();
-    activate(e.target);
+    activate($(e.target));
   })
 
-  function activate(selector) {
-    var target = $(selector);
+  function activate(target) {
     clearActiveSelectors();
     target.addClass('algolia-active-selector');
 
-    window.top.jQuery('#algolia-selector').val(computeSelectorPath(selector));
+    window.top.jQuery('#algolia-selector').val(computeSelectorPath(target));
   }
 
-  function computeSelectorPath(selector) {
-    var target = $(selector);
+  function computeSelectorPath(target) {
     var path = '';
 
     var selectorId = target.attr('id');
@@ -71,9 +77,15 @@ jQuery(function($) {
 
   function formatClasses(classes) {
     var pieces = classes.split(' ');
-    pieces.splice(pieces.indexOf('algolia-selector'), 1);
-    pieces.splice(pieces.indexOf('algolia-active-selector'), 1);
-
+    
+    var index = pieces.indexOf('algolia-selector');
+    if(index !== -1) {
+      pieces.splice(index, 1);
+    }
+    index = pieces.indexOf('algolia-active-selector');
+    if(index !== -1) {
+      pieces.splice(index, 1);
+    }
 
     if(pieces.length === 0) {
       return '';
