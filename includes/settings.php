b0f4bc91513a1326fb2aa62da6563412a784ae7e
Raymond Rutjes <raymond.rutjes@gmail.com>
2016-12-02 22:37:19 +0100

    Detect if admin pages are conrrectly configured
    Resolves: #28

--- includes/settings.php
+++ includes/settings.php
@@ -8,7 +8,7 @@ add_option( 'algolia_wc_primary_color', '#46AEDA' );
  * @return array
  */
 function aw_get_pages() {
-	return (array) get_option( 'algolia_wc_pages', array() );
+	return (array) get_option( 'algolia_wc_pages', array( 'category', 'tag', 'search' ) );
 }
 
 /**
@@ -27,6 +27,13 @@ function aw_set_pages( array $pages ) {
 }
 
 /**
+ * @return bool
+ */
+function aw_is_configured_pages() {
+	return count( aw_get_pages() ) > 0;
+}
+
+/**
  * @param string $selector
  */
 function aw_set_selector( $selector ) {
@@ -41,6 +48,14 @@ function aw_get_selector() {
 }
 
 /**
+ * @return bool
+ */
+function aw_is_configured_zoning() {
+	// Todo: Check in the output if the selector is present.
+	return mb_strlen( aw_get_selector() ) > 1;
+}
+
+/**
  * @return array
  */
 function aw_get_primary_color() {
@@ -48,10 +63,19 @@ function aw_get_primary_color() {
 }
 
 /**
+ * @param string $color
+ *
  * @return array
  */
 function aw_set_primary_color( $color ) {
 	// Todo: validate hexacode.
 	
 	update_option( 'algolia_wc_primary_color', $color );
+}
+
+/**
+ * @return bool
+ */
+function aw_is_configured_appearance() {
+	return mb_strlen( aw_get_primary_color() ) > 3;
 }
